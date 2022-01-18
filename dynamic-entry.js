e3788ddaf39690cf7968b80e0a293da712c497c7
Tom Coleman <tom@chromatic.com>
2022-01-18 11:27:12 +1100

    Accept changes in entry functions

--- dynamic-entry.js
+++ dynamic-entry.js
@@ -2,3 +2,6 @@ window.go = (name) =>
   /* webpackChunkName: "[request]" */
   /* webpackInclude: /(a|b).js$/ */
   import(`./${name}`);
+
+module.hot.accept('./a', () => console.log('new a'));
+module.hot.accept('./b', () => console.log('new a'));
--- static-entry.js
+++ static-entry.js
@@ -4,3 +4,6 @@ const imports = {
 };
 
 window.go = (name) => imports[name]();
+
+module.hot.accept('./a', () => console.log('new a'));
+module.hot.accept('./b', () => console.log('new a'));
